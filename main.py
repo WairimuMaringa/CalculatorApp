e180bd0e0cd6180bd2d55712ff5b2d1c4e557bcd
WairimuMaringa <makena96wairimu@gmail.com>
2023-02-22 11:23:37 -0800

    layout

--- main.py
+++ main.py
@@ -26,12 +26,23 @@ class MainCalc(App):
             h_layout = BoxLayout()
             for label in row:
                 button = Button(
-                    text = label, font_size=30, backgroun_color="grey",
+                    text = label, font_size=30, background_color="grey",
                     pos_hint={"center_x": 0.5, "center_y": 0.5},
                 )
+                button.bind(on_press=self.on_button_press)
 
                 h_layout.add_widget(button)
             main_layout.add_widget(h_layout)
+
+            equal_button = Button(
+                text = "=", font_size=30, background_color="grey",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
+            )
+
+            equal_button.bind(on_press=self.on_solution)
+            main_layout.add_widget(equal_button)
+
+            return main_layout
 
 
 if __name__ == "__main__":
