23a11128eed0bed2d0cb89fa37456ad671ea263b
WairimuMaringa <makena96wairimu@gmail.com>
2023-02-22 05:27:06 -0800

    slow and steady

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ from kivy.uix.textinout import TextInput
 class MainCalc(App):
     def build(self):
         self.operators = ["/", "*", "-", "+"]
+        self.last_was_operator = None
+        self.last_button = None
+
+        main_layout = BoxLayout(orientation = "vertical")
+
+        self.solution = TextInput(background_color = "black", foreground_color = "white")
+
+        main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "+"],
+            [".", "0", "C", "-"],
+        ]
+
+        for row in buttons:
 
 
 if __name__ == "__main__":
