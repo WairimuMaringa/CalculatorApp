c4244bde561c27d71c2192bae91d6c12b33ec269
WairimuMaringa <makena96wairimu@gmail.com>
2023-02-23 06:37:25 -0800

    added edits

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ class MainCalc(App):
         self.last_was_operator = None
         self.last_button = None
 
-        main_layout = BoxLayout(orientation = "vertical")
+        main_layout = BoxLayout(orientation="vertical")
 
-        self.solution = TextInput(background_color = "black", foreground_color = "white")
+        self.solution = TextInput(background_color="black",
+                                  foreground_color="white",
+                                  multiline=False,
+                                  halign="right", font_size=55, readonly=True)
 
         main_layout.add_widget(self.solution)
         buttons = [
@@ -25,19 +28,17 @@ class MainCalc(App):
         for row in buttons:
             h_layout = BoxLayout()
             for label in row:
-                button = Button(
-                    text = label, font_size=30, background_color="grey",
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
+                button = Button(text=label, font_size=30,
+                                background_color="grey",
+                                pos_hint={"center_x": 0.5, "center_y": 0.5})
                 button.bind(on_press=self.on_button_press)
 
                 h_layout.add_widget(button)
             main_layout.add_widget(h_layout)
 
-            equal_button = Button(
-                text = "=", font_size=30, background_color="grey",
-                pos_hint={"center_x": 0.5, "center_y": 0.5},
-            )
+            equal_button = Button(text="=", font_size=30,
+                                  background_color="grey",
+                                  pos_hint={"center_x": 0.5, "center_y": 0.5})
 
             equal_button.bind(on_press=self.on_solution)
             main_layout.add_widget(equal_button)
@@ -52,7 +53,7 @@ class MainCalc(App):
             self.solution.text = ""
         else:
             if current and (
-                self.last_was_operator and button_text in self.operators):
+               self.last_was_operator and button_text in self.operators):
                 return
             elif current == "" and button_text in self.operators:
                 return
